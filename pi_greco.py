37e75ff49d5720e9682cdec413e5267528ff1610
AntonioLegnante <antoniolegnante12@gmail.com>
2019-02-16 12:43:02 +0000

    pigreco

---
+++ pi_greco.py
@@ -0,0 +1,14 @@
+#calcolo del pigreco
+
+termini = int(input("inserisci il numero di termini da usare"))
+numeratore = -4
+pigreco = 4
+cont = 3
+
+for i in range(1, termini):
+    pigreco += numeratore / cont
+    numeratore = - numeratore
+    cont += 2
+
+print(pigreco)
+    
