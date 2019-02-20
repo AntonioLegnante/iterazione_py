ba2f613b62dca5428a14f0a133053ecbffd67fc5
AntonioLegnante <AntonioLegnante12@gmail.com>
2019-02-20 19:28:32 +0000

    corretto

--- pi_greco.py
+++ pi_greco.py
@@ -1,14 +1,16 @@
 #calcolo del pigreco
 
 termini = int(input("inserisci il numero di termini da usare"))
-numeratore = -4
-pigreco = 4
-cont = 3
+while termini < 0:
+    termini = int(input("inserisci il numero di termini da usare"))
+numeratore = -4.
+pigreco = 4.
+cont = 3.
 
 for i in range(1, termini):
     pigreco += numeratore / cont
     numeratore = - numeratore
-    cont += 2
+    cont += 2.
 
 print(pigreco)
     
