67524c487f7f5445a4c45ed1328e772b50f71b34
AntonioLegnante <AntonioLegnante12@gmail.com>
2019-02-20 19:23:49 +0000

    problemi inutili

--- tavola_pitagorica.py
+++ tavola_pitagorica.py
@@ -1,6 +1,8 @@
 #tavola pitagorica in python 
 
-for j in range(1, 13):
-    for i in range(1, 13):
-        print((i * j), end =' ')
+for riga in range(1, 11):
+    for colonna in range(1, 11):
+        print((riga * colonna), end =' ')
     print(" ")
+
+#ps che arriva fino a 12 non è che cambi poi più di tanto
